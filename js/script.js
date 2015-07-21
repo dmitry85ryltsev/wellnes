7cb1ec1c53c25c0c5194e5eaf30412cca8c7be3d
dmitry85ryltsev <ryltsev.dmitry@gmail.com>
2015-07-21 11:42:53 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+            $('#slides').slides({
+                preload: true,
+                preloadImage: 'img/loading.gif',
+                play: 4000,
+                pause: 2500,
+                hoverPause: true
+            });
+        });
